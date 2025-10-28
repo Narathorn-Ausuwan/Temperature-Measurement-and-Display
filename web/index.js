057362b20f3c0346d58dd5c81c04f28e159a40d8
MatchaDesu <matchadesu02@gmail.com>
2025-10-28 18:47:07 +0700

    first commit

---
+++ web/index.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const { InfluxDB, Point } = require('@influxdata/influxdb-client')
+const webpush = require('web-push')
+
+
+
+
+
+
