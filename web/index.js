0a5a1f9a59fbc0636a7662be9a338b999f2decf8
MatchaDesu <matchadesu02@gmail.com>
2025-11-02 18:13:36 +0700

    update imo

--- web/index.js
+++ web/index.js
@@ -3,20 +3,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const ejs = require('ejs');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const { createClient } = require('@supabase/supabase-js');
 const webpush = require('web-push');
 
 dotenv.config();
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.json());
-app.use(cookieParser());
 app.set('view engine', 'ejs');
-
-supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 const indexController = require('./controllers/indexController');
 
